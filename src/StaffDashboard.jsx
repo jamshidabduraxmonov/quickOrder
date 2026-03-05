befc1f68fc8d269c8ab7d8c83c6e52c1ccc45196
Jamshid <jamshidabduraxmonov164@gmail.com>
2026-03-05 22:03:32 +0400

    feat: implemented first stage admin dashboard logic and UI

--- src/StaffDashboard.jsx
+++ src/StaffDashboard.jsx
@@ -1,24 +1,70 @@
 import {collection, query, onSnapshot} from 'firebase/firestore';
 import {useState, useEffect} from 'react';
 import { db } from './firebase.js';
+import data from './products.json'
 
 
 const StaffDashboard = () => {
-    const [orders, setOrders] = useState([]);
+    const [orders, setOrders] = useState([]); // No_1 state
+    const [orderNames, setOrderNames] = useState([]); // No_2 State
+    console.log('Total Orders: ', orders);
 
     useEffect(() => {
         const ordersCollection = collection(db, 'orders');
+
         const unsubscribe = onSnapshot(ordersCollection, (snapshot)=> {
-            console.log("Current orders:", snapshot.docs.length);
+            let tempOrders = [];
+            let allNames = [];
+            snapshot.forEach((doc) => {
+                const realData = doc.data();
+                tempOrders.push({id: doc.id, ...realData});
+             });
+
+
+            tempOrders.forEach((order) => {
+                const ids = Object.keys(order.items);
+                ids.forEach((code) => {
+                   const product = data.find(food => food.id == code); 
+                   allNames.push(product.name);
+                   setOrderNames(allNames);
+                    }
+                );
+
+                   
+                });
+            
+            
+
+
          });
+
+        
+         
+
 
         return() => unsubscribe();
     }, []);
 
 
+
+
+
+
     return (
         <div>
             <h1>StaffDashboard</h1>
+
+            {
+                
+              orderNames.map((name) => {
+                
+                return(
+                    <p>{name}</p>
+                )
+              })
+
+                
+            }
         </div>
     );
 };
